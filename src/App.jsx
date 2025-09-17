9de3b17d5aca22d73c162a7504ea0cc5e9778df3
codejordy25 <jordytsimba11@gmail.com>
2025-09-17 13:57:14 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ManagTasks from "../src/components/tasks/ManagTasks";
+
+function App() {
+  return (
+    <div>
+      <ManagTasks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tasks/ManagTasks.jsx
@@ -0,0 +1,9 @@
+function ManagTasks() {
+  return (
+    <div>
+      <h2>Bienvenue Manager Tasks</h2>
+    </div>
+  );
+}
+
+export default ManagTasks;
