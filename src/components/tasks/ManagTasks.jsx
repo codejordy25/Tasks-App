03ad1479a07c612bc5cf68ff4546903813ccc01c
codejordy25 <jordytsimba11@gmail.com>
2025-09-18 06:07:25 +0200

    Config and replace TasksState

--- src/components/tasks/ManagTasks.jsx
+++ src/components/tasks/ManagTasks.jsx
@@ -5,11 +5,17 @@ import {
   deleteTask,
   getTasks,
 } from "..//..//services/tasks.service";
+import tasksReducer from "./tasks.reducer";
 
-const Tasks = () => {
+const ManagTasks = () => {
   const [inputValue, setInputValue] = useState("");
 
-  const [tasksState, dispatch] = useReducer(reducer, {});
+  const [tasksState, dispatch] = useReducer(tasksReducer, {
+    tasks: "",
+    count: 0,
+    loading: false,
+    error: null,
+  });
 
   // Définition des fonctions directement dans le composant
   const handleCreateTask = () => {
@@ -64,6 +70,55 @@ const Tasks = () => {
   }, []);
 
   const handleChangeInputValue = (e) => setInputValue(e.currentTarget.value);
+
+  return (
+    <>
+      <h1>Todo List with useReducer</h1>
+      <div style={{ display: "flex", gap: "8" }}>
+        <input
+          type="text"
+          onChange={handleChangeInputValue}
+          value={inputValue}
+        />
+        <button type="button" onClick={handleCreateTask}>
+          Create
+        </button>
+      </div>
+      <h2>
+        {tasksState.count} task{tasksState.count > 1 ? "s" : ""}
+      </h2>
+      {tasksState.loading ? <p>Loading...</p> : null}
+      {tasksState.error ? (
+        <p style={{ color: "red" }}>{tasksState.error}</p>
+      ) : null}
+      <ul
+        style={{
+          listStyleType: "none",
+          padding: 0,
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+        }}
+      >
+        {/* Le reste de la liste n'est pas visible */}
+
+        {tasksState.tasks.map((task) => (
+          <li
+            style={{
+              display: "flex",
+              gap: 8,
+              border: "1px solid #CFCFCF",
+              padding: 8,
+            }}
+            key={task.id}
+          >
+            <p style={{ margin: 0 }}>{task.title}</p>
+            {/* ... (le reste du code de la balise <li> n'est pas visible) */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-export default Tasks;
+export default ManagTasks;
